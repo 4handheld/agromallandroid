ac75dcbaafe96b99229a2b705c62c8a59fcec128
unknown <2handheld@gmail.com>
2020-05-21 15:35:23 +0100

    creaated CaptureDetailsFragment

--- app/src/main/java/com/handheld/agromalltest/fragments/farmer/CaptureDetailsFragment.java
+++ app/src/main/java/com/handheld/agromalltest/fragments/farmer/CaptureDetailsFragment.java
@@ -1,4 +1,4 @@
-package com.handheld.agromalltest.fragments;
+package com.handheld.agromalltest.fragments.farmer;
 
 import android.os.Bundle;
 
@@ -10,13 +10,12 @@ import android.view.ViewGroup;
 
 import com.handheld.agromalltest.R;
 
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CapturePhotographFragment#newInstance} factory method to
+ * Use the {@link CaptureDetailsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CapturePhotographFragment extends Fragment {
+public class CaptureDetailsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -26,7 +25,7 @@ public class CapturePhotographFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CapturePhotographFragment() {
+    public CaptureDetailsFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +35,11 @@ public class CapturePhotographFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CapturePhotographFragment.
+     * @return A new instance of fragment CaptureDetailsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CapturePhotographFragment newInstance(String param1, String param2) {
-        CapturePhotographFragment fragment = new CapturePhotographFragment();
+    public static CaptureDetailsFragment newInstance(String param1, String param2) {
+        CaptureDetailsFragment fragment = new CaptureDetailsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +60,6 @@ public class CapturePhotographFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_capture_photograph, container, false);
+        return inflater.inflate(R.layout.fragment_capture_details, container, false);
     }
 }
