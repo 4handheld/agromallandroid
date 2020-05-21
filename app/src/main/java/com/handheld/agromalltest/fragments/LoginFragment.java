4bb8fd6538470bc9b2d5cbb5c1f1f5622cd3020a
unknown <2handheld@gmail.com>
2020-05-21 15:26:36 +0100

    created DashBoardFragment

--- app/src/main/java/com/handheld/agromalltest/fragments/LoginFragment.java
+++ app/src/main/java/com/handheld/agromalltest/fragments/LoginFragment.java
@@ -1,4 +1,4 @@
-package com.handheld.agromalltest;
+package com.handheld.agromalltest.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.handheld.agromalltest.R;
 
 
 /**
